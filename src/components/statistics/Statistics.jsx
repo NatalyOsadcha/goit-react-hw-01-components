a97100d439227b69ab65ad1b48ef6824b0d4e5f4
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-04-30 17:20:06 +0300

    updated Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 export default function Statistics({ title, stats }) {
-  const styles = { backgroundColor: getRandomHexColor() };
-
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(stat => (
-          <li key={stat.id} className={css.item} style={styles}>
+          <li key={stat.id} className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
             <span className={css.label}>{stat.label}</span>
             <span className={css.percentage}>{stat.percentage}%</span>
           </li>
