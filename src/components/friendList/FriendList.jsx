cf3ec8e7ddd5ef76d1e4ca800f3a62b61bfccade
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-05-11 22:25:42 +0300

    updated friendListItem

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
-import FriendListItem from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 export default function FriendsList({ friends }) {
   return (
     <ul className={css.friendList}>
